d9b05ae5000ddd011929e9f085fb78279d29d9eb
Yelena Belikova <yelenabel@gmail.com>
2015-08-13 12:14:32 -0700

    uncommected velocity import

--- coin-return/src/main/java/CoinReturn.java
+++ coin-return/src/main/java/CoinReturn.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 
 import spark.ModelAndView;
 
-//import spark.template.velocity.VelocityTemplateEngine;
+import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 
