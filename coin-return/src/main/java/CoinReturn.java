2640fcf437ef01e894354a7e40fb0df8cce9a8a4
Yelena Belikova <yelenabel@gmail.com>
2015-08-13 09:47:23 -0700

    imported libraries

---
+++ coin-return/src/main/java/CoinReturn.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Arrays;
+import java.util.ArrayList;
+import static java.lang.System.out;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import spark.ModelAndView;
+
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+
+public class CoinReturn{
+  public static void main(String[] args) {
+    // this is where we'll put the templates
+  }
+
+  public static HashMap<String, Integer> returnChange (double change){
+    
+  }
+}
