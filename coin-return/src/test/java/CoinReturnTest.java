ad49f8aff6e322213e6e736637d40563fd8e95a9
Yelena Belikova <yelenabel@gmail.com>
2015-08-13 11:27:38 -0700

    passed test for user only entering an integer

--- coin-return/src/test/java/CoinReturnTest.java
+++ coin-return/src/test/java/CoinReturnTest.java
@@ -14,7 +14,20 @@ public class CoinReturnTest {
   @Test
   public void returnChange_CheckThatReturnsCorrectChange_true() {
     CoinReturn newReturn = new CoinReturn();
-    Integer numOne = 3;
-    assertEquals(numOne , newReturn.returnChange(90).get("quarter"));
+    Integer numThree = 3;
+    assertEquals(numThree , newReturn.returnChange(90).get("quarter"));
+    //return change is calling the method, 90 is the input
+  }
+  @Test
+  public void validateInput_MakeSureInputIsAnInt_true() {
+    CoinReturn newReturn = new CoinReturn();
+
+    assertEquals(true, newReturn.validateInput("23"));
+  }
+  @Test
+  public void validateInput_ReturnFalseWhenNotANum_false() {
+    CoinReturn newReturn = new CoinReturn();
+
+    assertEquals(false, newReturn.validateInput("peack0@ck"));
   }
 }
